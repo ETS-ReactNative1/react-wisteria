7734a8513067a0b4a492835a1b14b894560b6073
Islam Attrash <isattrash@gmail.com>
2020-04-23 10:40:02 +0300

    add react to externals

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'index.js',
+      libraryTarget: 'umd'
+    },
+    externals : {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        }
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        }
+      ]
+    }
+};
