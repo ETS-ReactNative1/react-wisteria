70c3f6f8c3681cf73b2bae72e5a98658d5f14f41
Islam Attrash <isattrash@gmail.com>
2020-05-13 15:39:37 +0300

    [FIX] Fix Storybook warnings (#22)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
+const webpackConfig = require('../webpack.config');
+
 module.exports = {
+    webpackFinal: (config) => {
+        return { ...config, module: { ...config.module, rules: webpackConfig.module.rules } };
+    },
     stories: ['../stories/**/decorator.js', '../stories/**/story.js']
 };
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
           test: /\.js$/,
           exclude: /node_modules/,
           use: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
         }
       ]
     }
