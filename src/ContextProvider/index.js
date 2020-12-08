eade7c09c4a7a8b7f57105202e5aff62328877f0
Islam Attrash <isattrash@gmail.com>
2020-12-08 18:22:11 +0200

    [FEAT] Auto Devtools (#65)

--- src/ContextProvider/index.js
+++ src/ContextProvider/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import useStateManagement from '../useStateManagement';
 
 export const TreeContext = React.createContext();
 
 const ContextProvider = ({
+    name = parseInt(1000 + Math.random() * 1000),
     Context,
     initialPropsMapper = (x) => x,
     derivedStateSyncers = [],
@@ -13,6 +14,19 @@ const ContextProvider = ({
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [context, setContext] = useStateManagement(initialPropsMapper(props), derivedStateSyncers, debug);
     effects.forEach((effect) => effect({ context, setContext }));
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const warnedAboutMissingDevToolRef = useRef();
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window._REACT_CONTEXT_DEVTOOL) {
+            window._REACT_CONTEXT_DEVTOOL({ id: name, displayName: name, values: context });
+        } else if (!warnedAboutMissingDevToolRef.current) {
+            warnedAboutMissingDevToolRef.current = true;
+            console.log('%cConsider installing "React Context DevTool" in order to inspect the Wisteria state', 'color:#1dbf73');
+        }
+    }, [context]);
 
     return (
         <TreeContext.Provider value={Context}>
