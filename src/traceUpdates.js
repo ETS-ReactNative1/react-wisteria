f0520111e7aa11012db44ab9160e90cc0c4cd0fa
Islam Attrash <isattrash@gmail.com>
2022-02-08 10:19:10 +0200

    8.1.3

--- src/traceUpdates.js
+++ src/traceUpdates.js
@@ -17,7 +17,7 @@ const traceUpdates = ({ name, path, value, isChanged }) => {
     if (!isInDebugMode()) { return; }
 
     const cleanPath = path.replace(new RegExp(TOKEN_HASH, 'g'), '');
-    console.groupCollapsed(`%c react-wisteria :: ${name} setContext Path "${cleanPath}"`, `color:${isChanged ? '#1dbf73' : '#93a3b4'}`);
+    console.groupCollapsed(`%c react-wisteria :: ${name} setContext Path "${cleanPath}" ${isChanged ? '' : ' [UNCHANGED]'}`, `color:${isChanged ? '#1dbf73' : '#93a3b4'}`);
     console.log({ value });
     console.trace();
     console.groupEnd();
